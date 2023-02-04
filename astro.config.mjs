b90fd544c37a8adfa98a1b7fac2c02de46332ff7
skeletonkbd <skeletonkbd@gmail.com>
2023-02-04 20:09:44 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react()
 	],
-	site: `https://doc.skeletonkbd.com`,
+	site: `https://docs.skeletonkbd.com`,
 
 });
